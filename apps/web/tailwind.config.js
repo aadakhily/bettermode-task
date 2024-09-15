d976bb5825ef3451e3f68f7dcca93b4aef929bb5
Amir Abbas Dakhily <a.a.dakhily@gmail.com>
2024-09-15 20:42:34 +0330

    feat: install and config vazirmatn font

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,6 +3,19 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: "650px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1496px",
+      },
+    },
+    fontFamily: {
+      sans: ["Vazirmatn", "ui-sans-serif", "system-ui"],
+    },
   },
   plugins: [],
 };
